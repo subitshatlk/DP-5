0c83bb7d5cc92bd015f443d9df8877f642bc1257
Subitsha <subitsha@Subitshas-MacBook-Air.local>
2024-07-26 00:02:37 -0600

    Done DP-5

---
+++ WordBreak.java
@@ -0,0 +1,31 @@
+//TC - O(n3)
+//SC - O(n)
+class Solution {
+    HashSet<String> hashSet;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if(s == null || s.length() == 0){
+            return false;
+        }
+        hashSet = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        // int i = 1;
+        // int j = 0;
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 0; j < i; j++){
+                if(dp[j] == true){
+                    if(hashSet.contains(s.substring(j,i))){
+                        dp[i] = true;
+                        break;
+                    }
+                }
+
+            }
+
+        }
+        
+       return dp[n]; 
+    }
+    
+}
